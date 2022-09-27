76b9dad69338b4d65cf3deaefb514bce2b84c818
Jorge Romero <jorge@daduic.com>
2022-09-27 20:49:25 +0200

    tutorial iii

--- cmd/root.go
+++ cmd/root.go
@@ -27,6 +27,8 @@ import (
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+
+	"daduic.com/spotify"
 )
 
 var cfgFile string
@@ -49,6 +51,9 @@ to quickly create a Cobra application.`,
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	message := spotify.Hello("Gladys")
+	fmt.Println(message)
+
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
